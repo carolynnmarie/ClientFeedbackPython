897c4b0d5301bf8f5063fdd074dc63072595f753
Carolynn Vansant <carolynn@Carolynns-MacBook-Pro.local>
2023-12-31 16:42:29 -0500

    db setup and select working

--- clientFeedback.py
+++ clientFeedback.py
@@ -5,26 +5,46 @@ import json
 import time
 import ssl
 
-class clientFeedback:
 
-    def seedDB(self):
-        conn = sqlite3.connect('feedback.sqlite')
-        cur = conn.cursor()
-        cur.execute('''CREATE TABLE Feedback (name TEXT,comment TEXT);''')
 
-        strData = open('feedback.json')
-        jsonData = json.loads(strData)
+def seedDB():
+    conn = sqlite3.connect('feedback.sqlite')
+    cur = conn.cursor()
+    cur.execute('DROP TABLE IF EXISTS Feedback')
+    cur.execute('CREATE TABLE Feedback (name TEXT, comment TEXT);')
 
-        for entry in jsonData:
-            name = entry[0]
-            comment = entry[1]
+    strData = open('feedback.json')
+    dataString = strData.read()
+    jsonData = json.loads(dataString)
 
-            cur.execute('INSERT INTO Feedback (name) VALUES( ? )', (name,))
-            cur.execute('INSERT INTO Feedback (comment) VALUES ( ? )', (comment,))
+    for entry in jsonData:
+        name = entry[0]
+        comment = entry[1]
 
-    def getFeedback(self):
-        conn = sqlite3.connect('feedback.sqlite')
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM Feedback')
-        feedback = cur.fetchall()
-        print(feedback)
+        cur.execute('INSERT INTO Feedback (name) VALUES( ? )', (name,))
+        cur.execute('INSERT INTO Feedback (comment) VALUES ( ? )', (comment,))
+        conn.commit()
+
+def getFeedback():
+    conn = sqlite3.connect('feedback.sqlite')
+    cur = conn.cursor()
+    feedback = cur.execute('SELECT * FROM Feedback')
+    for row in feedback:
+        print(row[0],row[1])
+    cur.close()
+
+def getFeedbackFromKeyword():
+    conn = sqlite3.connect('feedback.sqlite')
+    cur = conn.cursor()
+    value = input("Enter keyword for search: ")
+    feedback = cur.execute('SELECT * FROM Feedback WHERE comment IN ( ? )', (value,))
+    for row in feedback:
+        print(row[0],row[1])
+    cur.close()
+
+
+
+
+if __name__ == '__main__':
+    seedDB()
+    getFeedbackFromKeyword()
