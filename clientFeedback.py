01dd18e871d77e956d60bdd010013a4db1f53e17
Carolynn Vansant <carolynn@Carolynns-MacBook-Pro.local>
2023-12-31 21:38:51 -0500

    search by keyword function working properly now

--- clientFeedback.py
+++ clientFeedback.py
@@ -10,19 +10,18 @@ import ssl
 def seedDB():
     conn = sqlite3.connect('feedback.sqlite')
     cur = conn.cursor()
-    cur.execute('DROP TABLE IF EXISTS Feedback')
+    cur.execute('DROP TABLE IF EXISTS Feedback;')
     cur.execute('CREATE TABLE Feedback (name TEXT, comment TEXT);')
 
     strData = open('feedback.json')
-    dataString = strData.read()
-    jsonData = json.loads(dataString)
+    jsonData = json.loads(strData.read())
 
     for entry in jsonData:
         name = entry[0]
         comment = entry[1]
 
-        cur.execute('INSERT INTO Feedback (name) VALUES( ? )', (name,))
-        cur.execute('INSERT INTO Feedback (comment) VALUES ( ? )', (comment,))
+        cur.execute('''INSERT INTO Feedback (name, comment ) VALUES( ?, ? )''', (name, comment))
+        #cur.execute('INSERT INTO Feedback (comment) VALUES ( ? )', (comment,))
         conn.commit()
 
 def getFeedback():
@@ -30,16 +29,25 @@ def getFeedback():
     cur = conn.cursor()
     feedback = cur.execute('SELECT * FROM Feedback')
     for row in feedback:
-        print(row[0],row[1])
+        if row[0] is not None:
+            print(row[0])
+        if row[1] is not None:
+            print(row[1])
     cur.close()
+
 
 def getFeedbackFromKeyword():
     conn = sqlite3.connect('feedback.sqlite')
     cur = conn.cursor()
     value = input("Enter keyword for search: ")
-    feedback = cur.execute('SELECT * FROM Feedback WHERE comment IN ( ? )', (value,))
+    str = '%' + value + '%'
+    cur.execute('SELECT name, comment FROM Feedback WHERE comment LIKE ( ? )', (str,))
+    feedback = cur.fetchall()
     for row in feedback:
-        print(row[0],row[1])
+        if row[0] is not None:
+            print(row[0])
+        if row[1] is not None:
+            print(row[1])
     cur.close()
 
 
